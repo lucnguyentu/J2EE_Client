1cc26b854ec570f2f5f098294439c611a2862ab8
lucnguyentu <lucnguyentu77@gmail.com>
2023-09-26 21:58:56 +0700

    checking

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Pages from "./pages/Pages"
 import Data from "./components/Data"
 import Cart from "./common/Cart/Cart"
 import Footer from "./common/footer/Footer"
+// import Sdata from "./components/shops/Sdata"
 import Sdata from "./components/shops/Sdata"
 
 function App() {
